2d86c33e5a3c8c367f72ca747b258099f45e475c
Yakiyo <yakiyo1206@gmail.com>
2023-07-28 08:22:22 +0000

    feat: omit empty for certain parts

--- parser/struct.go
+++ parser/struct.go
@@ -5,9 +5,9 @@ type RugFile struct {
 	// Run pre/post hooks
 	Hooks bool `json:"hooks"`
 	// A map of environment variables
-	Env map[string]string `json:"env"`
+	Env map[string]string `json:"env,omitempty"`
 	// paths to add to $PATH before running
-	Path []string `json:"path"`
+	Path []string `json:"path,omitempty"`
 	// List of scripts
 	Scripts map[string]string `json:"scripts"`
 }
