bde6a3d350a99d66050b05c045c37c779e5adee7
Yakiyo <yakiyo1206@gmail.com>
2023-07-28 10:13:03 +0000

    feat: script runner

--- cmd/run.go
+++ cmd/run.go
@@ -8,16 +8,47 @@ import (
 	"github.com/Yakiyo/rug/parser"
 )
 
-func Run(rug parser.RugFile, script string, args []string) (int, error) {
+func Run(rug *parser.RugFile, script string, args []string) (int, error) {
 	rugScript := rug.GetScript(script)
+
+	if rug.Hooks && rug.HasScript("pre"+script) {
+		pre := command(rug, rug.GetScript("pre"+script))
+		err := pre.Run()
+		if err != nil {
+			return handleErr(err)
+		}
+	}
 
 	fullCommand := strings.Join([]string{rugScript, strings.Join(args, " ")}, " ")
 
-	c := exec.Command("sh", "-c", fullCommand)
+	c := command(rug, fullCommand)
+
+	err := c.Run()
+
+	if err != nil {
+		return handleErr(err)
+	}
+
+	if rug.Hooks && rug.HasScript("post"+script) {
+		post := command(rug, rug.GetScript("post"+script))
+		err := post.Run()
+		if err != nil {
+			return handleErr(err)
+		}
+	}
+	return 0, nil
+}
+
+func command(rug *parser.RugFile, args ...string) *exec.Cmd {
+	c := exec.Command("sh", "-c", strings.Join(args, " "))
+	c.Env = append(os.Environ(), rug.GetEnv()...)
 	c.Stdout = os.Stdout
 	c.Stderr = os.Stderr
 	c.Stdin = os.Stdin
-	err := c.Run()
+	return c
+}
+
+func handleErr(err error) (int, error) {
 	if err == nil {
 		return 0, nil
 	}
