f4eb0b1cf3e94aaeb8584cf9f9cb3d16e7b3119f
Yakiyo <yakiyo1206@gmail.com>
2023-07-28 06:49:24 +0000

    feat: default rugfile

---
+++ parser/default.go
@@ -0,0 +1,25 @@
+package parser
+
+import (
+	"os"
+
+	json "github.com/json-iterator/go"
+)
+
+// Default rugfile values
+func DefaultRug() RugFile {
+	return RugFile{
+		Scripts: map[string]string{
+			"test": "echo Hello World",
+		},
+	}
+}
+
+func WriteDefault(path string) error {
+	def, err := json.Marshal(DefaultRug())
+	if err != nil {
+		return err
+	}
+	err = os.WriteFile(path, def, 0666)
+	return err
+}
