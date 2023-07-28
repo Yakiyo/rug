4933ae150a94a5b6c04df4b0eb8e383e0be3af0a
Yakiyo <yakiyo1206@gmail.com>
2023-07-28 06:31:50 +0000

    feat: base cmd

--- cmd/root.go
+++ cmd/root.go
@@ -1,20 +1,46 @@
 package cmd
 
 import (
+	"fmt"
 	"os"
 
 	cc "github.com/ivanpirog/coloredcobra"
+	"github.com/samber/lo"
 	"github.com/spf13/cobra"
 )
 
 var rootCmd = &cobra.Command{
 	Use:   "rug",
-	Short: "Dead simple script runner",
-	Long: `Rug is an easy to use and simplified command runner.
-	
-No complicated setup like Make or Cmake. Rug is inspired by npm scripts. 
-For details visit https://github.com/Yakiyo/rug`,
-	Run: func(cmd *cobra.Command, args []string) {},
+	Short: shortDesc,
+	Long:  longDesc,
+	Run: func(cmd *cobra.Command, args []string) {
+		// Check for flags first
+
+		if lo.Must(cmd.Flags().GetBool("version")) {
+			fmt.Printf("Rug v%v\n", Version)
+			return
+		}
+
+		if lo.Must(cmd.Flags().GetBool("list")) {
+			// TODO
+			return
+		}
+
+		if lo.Must(cmd.Flags().GetBool("init")) {
+			// TODO
+			return
+		}
+
+		// Now check for scripts
+
+		// If theres no args, show help and exit
+		if len(args) < 1 {
+			cmd.Help()
+			os.Exit(1)
+		}
+
+		// TODO
+	},
 }
 
 func Execute() {
@@ -39,4 +65,8 @@ func Execute() {
 
 func init() {
 	rootCmd.Flags().BoolP("version", "v", false, "rug version")
+	rootCmd.Flags().BoolP("list", "l", false, "list available scripts")
+	rootCmd.Flags().BoolP("init", "i", false, "init rug file")
+
+	rootCmd.Flags().StringP("config", "c", "rug.json", "path to rug file")
 }
